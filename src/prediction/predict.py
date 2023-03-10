0c5a2f2659703b9faee30cfd3ee2a0049e7c1950
clementreiffers <reiffersclement@gmail.com>
2023-03-10 13:35:02 +0100

    dockerfile qui fonctionne mais ecr pas encore verifié

--- src/prediction/predict.py
+++ src/prediction/predict.py
@@ -1,16 +1,18 @@
-from tensorflow.keras.models import load_model
-from pandas import json_normalize
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-import boto3
 import zipfile
 
+import boto3
+import numpy as np
+from pandas import json_normalize
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import load_model
+
+
 def recup_model():
-    s3 = boto3.resource('s3')
-    s3.meta.client.download_file('test-bucket-mmarcelin', 'model_lstm.zip', '/tmp/model_lstm.zip')
-    file = zipfile.ZipFile('/tmp/model_lstm.zip', 'r')
-    reconstructed_model = load_model(file)
-    return reconstructed_model
+    s3 = boto3.resource("s3")
+    s3.meta.client.download_file(
+        "test-bucket-mmarcelin", "model_lstm.zip", "/tmp/model_lstm.zip"
+    )
+    return load_model(zipfile.ZipFile("/tmp/model_lstm.zip", "r"))
 
 
 def pred(x, model):
@@ -21,33 +23,27 @@ def pred(x, model):
     pred = model.predict(x_reshape)
     return np.argmax(pred)
 
+
 def lambda_handler():
     # récuperer le message JSON
-    sqs = boto3.client('sqs')
-    queue_url = 'SQS_QUEUE_URL'
+    sqs = boto3.client("sqs")
     response = sqs.receive_message(
         QueueUrl="https://sqs.eu-west-1.amazonaws.com/715437275066/queue_ia_mm.fifo",
-        AttributeNames=[
-            'SentTimestamp'
-        ],
+        AttributeNames=["SentTimestamp"],
         MaxNumberOfMessages=1,
-        MessageAttributeNames=[
-            'All'
-        ],
+        MessageAttributeNames=["All"],
         VisibilityTimeout=0,
-        WaitTimeSeconds=0
+        WaitTimeSeconds=0,
     )
     message = response["Messages"]
-    i = 0
     for i in range(len(message)):
         data = message[i]["Body"]
-        x = json_normalize(data['data'])
+        x = json_normalize(data["data"])
         # récupérer model sur S3
         model = recup_model()
         # lancer la prediction
         prediction = pred(x, model)
         sqs.delete_message(
             QueueUrl="https://sqs.eu-west-1.amazonaws.com/715437275066/queue_ia_mm.fifo",
-            ReceiptHandle=message[i]['ReceiptHandle']
+            ReceiptHandle=message[i]["ReceiptHandle"],
         )
-
